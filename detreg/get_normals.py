07238f22086c990cc4e068b2171494a73fb9b9b4
Shalaka Vaidya <shalakavaidya@a-10-27-11-139.dynapool.vpn.nyu.edu>
2022-04-17 10:48:10 -0400

    normals

---
+++ detreg/get_normals.py
@@ -0,0 +1,42 @@
+import os
+from statistics import mean
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+
+
+from dataset import UnlabeledDataset, LabeledDataset
+
+def main():
+    train_dataset = UnlabeledDataset(root='/unlabeled', transform=torchvision.transforms.ToTensor())
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=8)
+
+    r_means=[]
+    b_means=[]
+    g_means=[]
+    r_stds=[]
+    g_stds=[]
+    b_stds=[]
+    for i, output in enumerate(train_loader):
+        image = output[0].type(torch.DoubleTensor) 
+        r_means.append(image[0].mean())
+        b_means.append(image[1].mean())
+        g_means.append(image[2].mean())
+        r_stds.append(image[0].std())
+        b_stds.append(image[1].std())
+        g_stds.append(image[2].std())
+        print(i)
+        if i==10:
+            break
+    print("1:", r_means.mean(), r_stds.mean)
+    print("2:", b_means.mean(), b_stds.mean)
+    print("3:", g_means.mean(), g_stds.mean)
+
+    
+ 
+    
+if __name__ == "__main__":
+    main()
