5865de296cdb12eac1dba2f9bddbf0fc68fe5dfe
Shalaka Vaidya <shalakavaidya@b-10-27-82-230.dynapool.vpn.nyu.edu>
2022-04-17 18:16:10 -0400

    checking original code

---
+++ detreg/test_training.py
@@ -0,0 +1,33 @@
+import pytorch_lightning as pl
+from pl_bolts.models.self_supervised import SwAV
+from pl_bolts.datamodules import STL10DataModule
+from pl_bolts.models.self_supervised.swav.transforms import (
+    SwAVTrainDataTransform, SwAVEvalDataTransform
+)
+from pl_bolts.transforms.dataset_normalizations import stl10_normalization
+
+# data
+batch_size = 128
+dm = STL10DataModule(data_dir='.', batch_size=batch_size)
+dm.train_dataloader = dm.train_dataloader_mixed
+dm.val_dataloader = dm.val_dataloader_mixed
+
+dm.train_transforms = SwAVTrainDataTransform(
+    normalize=stl10_normalization()
+)
+
+dm.val_transforms = SwAVEvalDataTransform(
+    normalize=stl10_normalization()
+)
+
+# model
+model = SwAV(
+    gpus=1,
+    num_samples=dm.num_unlabeled_samples,
+    dataset='stl10',
+    batch_size=batch_size
+)
+
+# fit
+trainer = pl.Trainer(default_root_dir='./model',precision=16, max_epochs=5)
+trainer.fit(model)
