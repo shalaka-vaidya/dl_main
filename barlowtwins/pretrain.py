bac3fd377012c77293631de1205f6c110b081efc
Shalaka Vaidya <shalakavaidya@a-10-27-11-235.dynapool.vpn.nyu.edu>
2022-04-22 20:40:30 -0400

    bringing code back

--- barlowtwins/pretrain.py
+++ barlowtwins/pretrain.py
@@ -20,18 +20,18 @@ def main():
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=False, num_workers=8)
     print("got data")
 
-    # file paths
-    # save_path = './'
-    # file_name = "checkpoint.pth"
+    #file paths
+    save_path = './'
+    file_name = "checkpoint.pth"
 
-    # # Model
-    # project_dimension = 2048
-    # resnet = torchvision.models.resnet50()
-    # backbone = nn.Sequential(*list(resnet.children())[:-1])
-    # model = BarlowTwins(backbone, project_dimension)
+    # Model
+    project_dimension = 2048
+    resnet = torchvision.models.resnet50()
+    backbone = nn.Sequential(*list(resnet.children())[:-1])
+    model = BarlowTwins(backbone, project_dimension)
 
-    # device = "cuda" if torch.cuda.is_available() else "cpu"
-    # model.to(device)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
 
     # # Data 
     # # data_location = '/tmp/unlabeled'
@@ -40,11 +40,11 @@ def main():
     # #     torchvision.transforms.ToTensor()
     # # ])
 
-    # #data = LabeledDataset(root=data_location,split=split,transforms=train_transform)
-    # #dataset = LightlyDataset.from_torch_dataset(data, transform=train_transform)
-    # # dataset = LightlyDataset(
-    # #     input_dir=data_location
-    # # )
+    #data = LabeledDataset(root=data_location,split=split,transforms=train_transform)
+    #dataset = LightlyDataset.from_torch_dataset(data, transform=train_transform)
+    # dataset = LightlyDataset(
+    #     input_dir=data_location
+    # )
 
     # # collate_fn = ImageCollateFunction(input_size=input_dimension)
     # # dataloader = torch.utils.data.DataLoader(
@@ -70,26 +70,26 @@ def main():
     # dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=input_dimension, shuffle=False, num_workers=8)
 
 
-    # criterion = BarlowTwinsLoss()
-    # optimizer = torch.optim.SGD(model.parameters(), lr=0.06)
+    criterion = BarlowTwinsLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.06)
 
-    # print("Starting Training")
-    # for epoch in range(2):
-    #     total_loss = 0
-    #     for (x0, x1), _, _ in dataloader:
-    #         x0 = x0.to(device)
-    #         x1 = x1.to(device)
-    #         z0 = model(x0)
-    #         z1 = model(x1)
-    #         loss = criterion(z0, z1)
-    #         total_loss += loss.detach()
-    #         loss.backward()
-    #         optimizer.step()
-    #         optimizer.zero_grad()
-    #     avg_loss = total_loss / len(dataloader)
-    #     print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
+    print("Starting Training")
+    for epoch in range(2):
+        total_loss = 0
+        for (x0, x1), _, _ in train_loader:
+            x0 = x0.to(device)
+            x1 = x1.to(device)
+            z0 = model(x0)
+            z1 = model(x1)
+            loss = criterion(z0, z1)
+            total_loss += loss.detach()
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        avg_loss = total_loss / len(train_loader)
+        print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
 
-    # torch.save(model.state_dict(), f"{save_path}/{file_name}")
+    torch.save(model.state_dict(), f"{save_path}/{file_name}")
 
 
     # def main():
