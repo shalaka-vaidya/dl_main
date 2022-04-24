7b2932a4c7ed0a70d365af7fbee5e085ac92ca7a
Shalaka Vaidya <shalakavaidya@b-10-27-86-169.dynapool.vpn.nyu.edu>
2022-04-24 12:58:50 -0400

    hope it works

--- barlowtwins/busy.py
+++ barlowtwins/busy.py
@@ -1,7 +1,7 @@
 import torch
 
-a = torch.randn(10000, 10000).cuda()
-b = torch.randn(10000, 10000).cuda()
+a = torch.randn(100000, 100000).cuda()
+b = torch.randn(100000, 100000).cuda()
 while True:
 	ans = a.mm(b.t()).cuda()
 	del ans
