78ae92e5f111d655e2498b4fccec9cb49ec30f08
Shalaka Vaidya <shalakavaidya@a-10-27-20-169.dynapool.vpn.nyu.edu>
2022-05-01 08:01:22 -0400

    yolo

--- yolov5-master/eval.py
+++ yolov5-master/eval.py
@@ -1,7 +1,8 @@
 import torch
 from dataset import LabeledDataset, UnlabeledDataset
 import transforms as T
-import utils
+import torchvision.transforms as s
+from PIL import Image
 
 def collate_fn(batch):
     return tuple(zip(*batch))
@@ -14,11 +15,13 @@ def get_transform(train):
     return T.Compose(transforms)
 
 valid_dataset = LabeledDataset(root='/labeled', split="validation", transforms=get_transform(train=False))
-valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=5, shuffle=False, num_workers=4, collate_fn=collate_fn)
+valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=4, collate_fn=collate_fn)
 count=0
+transform = s.ToPILImage()
 for img,trg in valid_loader:
     count+=1
     if count==3:
         break
-    print(img[0].shape)
+    img = transform(img[0])
+    img.save("test"+str(count)+".jpg")
     print(trg[0])
