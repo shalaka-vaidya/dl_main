c846735c5e3d1d1a0ad876981ea483c9881fd579
Shalaka Vaidya <shalakavaidya@a-10-27-20-169.dynapool.vpn.nyu.edu>
2022-05-01 07:40:00 -0400

    yolo

---
+++ yolov5-master/eval.py
@@ -0,0 +1,20 @@
+import torch
+from dataset import LabeledDataset, UnlabeledDataset
+import transforms as T
+import utils
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+valid_dataset = LabeledDataset(root='/labeled', split="validation", transforms=get_transform(train=False))
+valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=2, shuffle=False, num_workers=4, collate_fn=utils.collate_fn)
+count=0
+for img,trg in valid_loader(valid_loader):
+    count+=1
+    if count==3:
+        break
+    print(img.shape,trg.shape)
