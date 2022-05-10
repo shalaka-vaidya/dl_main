f54c6a03e7b9acbb774c017c7b0e33e031b0ddd1
Shalaka Vaidya <shalakavaidya@Shalakas-MBP.lan>
2022-05-10 14:25:12 -0400

    class

--- final_submission/coco_eval.py
+++ final_submission/coco_eval.py
@@ -26,12 +26,13 @@ class CocoEvaluator(object):
         self.coco_eval = {}
         for iou_type in iou_types:
             self.coco_eval[iou_type] = COCOeval(coco_gt, iouType=iou_type)
-            self.coco_eval[iou_type].params=[param]
+            #self.coco_eval[iou_type].params=[param]
         self.img_ids = []
         self.eval_imgs = {k: [] for k in iou_types}
 
     def update(self, predictions):
-        img_ids = list(np.unique(list(predictions.keys())))
+        #img_ids = list(np.unique(list(predictions.keys())))
+        img_ids = [1,2]
         self.img_ids.extend(img_ids)
 
         for iou_type in self.iou_types:
