5514c41ec8797fd626bb34bca78e541be317108a
Shalaka Vaidya <shalakavaidya@a-10-27-26-5.dynapool.vpn.nyu.edu>
2022-05-10 12:00:24 -0400

    class

--- final_submission/coco_eval.py
+++ final_submission/coco_eval.py
@@ -26,7 +26,7 @@ class CocoEvaluator(object):
         self.coco_eval = {}
         for iou_type in iou_types:
             self.coco_eval[iou_type] = COCOeval(coco_gt, iouType=iou_type)
-            self.coco_eval.params=[param]
+            self.coco_eval[iou_type].params=[param]
         self.img_ids = []
         self.eval_imgs = {k: [] for k in iou_types}
 
