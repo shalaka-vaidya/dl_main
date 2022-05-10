674fb0449af2ad6c693efc65e50399e4bf6a8c58
Shalaka Vaidya <shalakavaidya@b-10-27-90-137.dynapool.vpn.nyu.edu>
2022-05-10 19:06:21 -0400

    class

--- final_submission/engine.py
+++ final_submission/engine.py
@@ -9,6 +9,109 @@ from coco_utils import get_coco_api_from_dataset
 from coco_eval import CocoEvaluator
 import utils
 
+class_dict = {
+'cup or mug': 0,
+'bird': 1,
+'hat with a wide brim': 2,
+'person': 3,
+'dog': 4,
+'lizard': 5,
+'sheep': 6,
+'wine bottle': 7,
+'bowl': 8,
+'airplane': 9,
+'domestic cat': 10,
+'car': 11,
+'porcupine': 12,
+'bear': 13,
+'tape player': 14,
+'ray': 15,
+'laptop': 16,
+'zebra': 17,
+'computer keyboard': 18,
+'pitcher': 19,
+'artichoke': 20,
+'tv or monitor': 21,
+'table': 22,
+'chair': 23,
+'helmet': 24,
+'traffic light': 25,
+'red panda': 26,
+'sunglasses': 27,
+'lamp': 28,
+'bicycle': 29,
+'backpack': 30,
+'mushroom': 31,
+'fox': 32,
+'otter': 33,
+'guitar': 34,
+'microphone': 35,
+'strawberry': 36,
+'stove': 37,
+'violin': 38,
+'bookshelf': 39,
+'sofa': 40,
+'bell pepper': 41,
+'bagel': 42,
+'lemon': 43,
+'orange': 44,
+'bench': 45,
+'piano': 46,
+'flower pot': 47,
+'butterfly': 48,
+'purse': 49,
+'pomegranate': 50,
+'train': 51,
+'drum': 52,
+'hippopotamus': 53,
+'ski': 54,
+'ladybug': 55,
+'banana': 56,
+'monkey': 57,
+'bus': 58,
+'miniskirt': 59,
+'camel': 60,
+'cream': 61,
+'lobster': 62,
+'seal': 63,
+'horse': 64,
+'cart': 65,
+'elephant': 66,
+'snake': 67,
+'fig': 68,
+'watercraft': 69,
+'apple': 70,
+'antelope': 71,
+'cattle': 72,
+'whale': 73,
+'coffee maker': 74,
+'baby bed': 75,
+'frog': 76,
+'bathing cap': 77,
+'crutch': 78,
+'koala bear': 79,
+'tie': 80,
+'dumbbell': 81,
+'tiger': 82,
+'dragonfly': 83,
+'goldfish': 84,
+'cucumber': 85,
+'turtle': 86,
+'harp': 87,
+'jellyfish': 88,
+'swine': 89,
+'pretzel': 90,
+'motorcycle': 91,
+'beaker': 92,
+'rabbit': 93,
+'nail': 94,
+'axe': 95,
+'salt or pepper shaker': 96,
+'croquet ball': 97,
+'skunk': 98,
+'starfish': 99
+}
+
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
     model.train()
@@ -53,6 +156,78 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
     return metric_logger
+
+def summarize(self, catId=None):
+    """
+    Compute and display summary metrics for evaluation results.
+    Note this functin can *only* be applied on the default parameter setting
+    """
+
+    def _summarize(ap=1, iouThr=None, areaRng='all', maxDets=100):
+        p = self.params
+        iStr = ' {:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
+        titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
+        typeStr = '(AP)' if ap == 1 else '(AR)'
+        iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
+            if iouThr is None else '{:0.2f}'.format(iouThr)
+
+        aind = [i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng]
+        mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
+
+        if ap == 1:
+            # dimension of precision: [TxRxKxAxM]
+            s = self.eval['precision']
+            # IoU
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+
+            if isinstance(catId, int):
+                s = s[:, :, catId, aind, mind]
+            else:
+                s = s[:, :, :, aind, mind]
+
+        else:
+            # dimension of recall: [TxKxAxM]
+            s = self.eval['recall']
+            if iouThr is not None:
+                t = np.where(iouThr == p.iouThrs)[0]
+                s = s[t]
+
+            if isinstance(catId, int):
+                s = s[:, catId, aind, mind]
+            else:
+                s = s[:, :, aind, mind]
+
+        if len(s[s > -1]) == 0:
+            mean_s = -1
+        else:
+            mean_s = np.mean(s[s > -1])
+
+        print_string = iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s)
+        return mean_s, print_string
+
+    stats, print_list = [0] * 12, [""] * 12
+    stats[0], print_list[0] = _summarize(1)
+    stats[1], print_list[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
+    stats[2], print_list[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
+    stats[3], print_list[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
+    stats[4], print_list[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
+    stats[5], print_list[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
+    stats[6], print_list[6] = _summarize(0, maxDets=self.params.maxDets[0])
+    stats[7], print_list[7] = _summarize(0, maxDets=self.params.maxDets[1])
+    stats[8], print_list[8] = _summarize(0, maxDets=self.params.maxDets[2])
+    stats[9], print_list[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
+    stats[10], print_list[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
+    stats[11], print_list[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+
+    print_info = "\n".join(print_list)
+
+    if not self.eval:
+        raise Exception('Please run accumulate() first')
+
+    return stats, print_info
+
 
 
 def _get_iou_types(model):
@@ -105,7 +280,24 @@ def evaluate(model, data_loader, device):
     # accumulate predictions from all images
     coco_evaluator.accumulate()
     coco_evaluator.summarize()
+
     coco_eval = coco_evaluator.coco_eval["bbox"]
+    coco_stats, print_coco = summarize(coco_eval)
+    voc_map_info_list = []
+    for k,v in class_dict.items():
+        stats, _ = summarize(coco_eval, catId=v)
+        voc_map_info_list.append(" {:15}: {}".format(k, stats[1]))
+
+    print_voc = "\n".join(voc_map_info_list)
+    print(print_voc)
+    with open("record_mAP.txt", "w") as f:
+        record_lines = ["COCO results:",
+                        print_coco,
+                        "",
+                        "mAP(IoU=0.5) for each category:",
+                        print_voc]
+        f.write("\n".join(record_lines))
+
     print("HUSHHH")
     torch.set_num_threads(n_threads)
     return coco_evaluator
